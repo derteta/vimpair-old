a7533847b8ba573c1da8c012d9a25c1df3455b6d
derteta <derteta@gmx.de>
2016-10-05 22:34:19 +0200

    Refactor: Pull setting up a sending thread into its own class

---
+++ ftplugin/python/runloop.py
@@ -0,0 +1,28 @@
+import time
+import threading
+
+
+class Runloop(object):
+
+    def __init__(self, setup=None, process=None, cleanup=None, *a, **k):
+        self._setup = setup
+        self._process = process
+        self._cleanup = cleanup
+        self._thread = threading.Thread(target=self._run)
+
+    def start(self):
+        self._is_serving = True
+        self._thread.start()
+
+    def stop(self):
+        self._is_serving = False
+        self._thread.join()
+
+    def _run(self):
+        time.sleep(.5)
+        try:
+            self._setup()
+            while (self._is_serving):
+                self._process()
+        finally:
+            self._cleanup()
--- ftplugin/python/vimpair_server.py
+++ ftplugin/python/vimpair_server.py
@@ -1,43 +1,34 @@
 import vim
-import time
-import threading
 import Queue
 
 from connection import ServerConnection
+from runloop import Runloop
 from vim_interface import VimInterface
-
 
 class VimpairServer(object):
 
     def __init__(self, *a, **k):
         self._editor_interface = VimInterface(vim=vim)
         self._connection = ServerConnection()
-        self._thread = threading.Thread(target=self._serve)
+        self._runloop = Runloop(
+            setup=self._connection.connect,
+            process=self._serve,
+            cleanup=self._connection.disconnect
+        )
         self._queue = Queue.Queue()
 
     def start(self):
-        self._is_serving = True
-        self._thread.start()
+        self._runloop.start()
         self.update()
 
     def stop(self):
-        self._is_serving = False
-        self._thread.join()
+        self._runloop.stop()
 
     def update(self):
         self._queue.put_nowait(self._editor_interface.current_contents)
 
-    def _run_serve_loop(self):
-        while (self._is_serving):
-            try:
-                self._connection.send(self._queue.get(block=True, timeout=1))
-            except Queue.Empty:
-                pass
-
     def _serve(self):
-        time.sleep(.5)
         try:
-            self._connection.connect()
-            self._run_serve_loop()
-        finally:
-            self._connection.disconnect()
+            self._connection.send(self._queue.get(block=True, timeout=1))
+        except Queue.Empty:
+            pass
