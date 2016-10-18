8c4a216873881d33c5faf42bffed7205a593534f
derteta <derteta@gmx.de>
2016-10-18 22:10:59 +0200

    Add `cursor_position` property to `VimInterface`
    which normalizes the coordinates to both be 0-based

--- ftplugin/python/tests/vim_interface_tests.py
+++ ftplugin/python/tests/vim_interface_tests.py
@@ -8,6 +8,8 @@ def create_mock_vim(contents):
     mock_vim = Mock()
     mock_vim.current = Mock()
     mock_vim.current.buffer = contents
+    mock_vim.current.window = Mock()
+    mock_vim.current.window.cursor = (1,0)
     return mock_vim
 
 
@@ -27,3 +29,17 @@ class VimInterfaceTests(unittest.TestCase):
             interface.current_contents,
             'This is the first line.\nThis is the second line.'
         )
+
+    def test_returns_normalised_cursor_position(self):
+        mock_vim = create_mock_vim(['Only one line'])
+        interface = VimInterface(vim=mock_vim)
+
+        self.assertEqual(interface.cursor_position, (0, 0))
+
+    def test_updates_cursor_position(self):
+        mock_vim = create_mock_vim(['Only one line'])
+        interface = VimInterface(vim=mock_vim)
+
+        mock_vim.current.window.cursor = (11, 10)
+
+        self.assertEqual(interface.cursor_position, (10, 10))
--- ftplugin/python/vim_interface.py
+++ ftplugin/python/vim_interface.py
@@ -7,3 +7,9 @@ class VimInterface(object):
     @property
     def current_contents(self):
         return reduce(lambda s1, s2: s1 + '\n' + s2, self._vim.current.buffer[:])
+
+    @property
+    def cursor_position(self):
+        # Vim counts rows 1-based, but columns 0-based, we unify this
+        position = self._vim.current.window.cursor
+        return (position[0] - 1, position[1])
